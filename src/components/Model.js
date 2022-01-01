848f03b2b405e5c2c78810fd7f3278996c7e9f14
swmm-js <88747437+swmm-js@users.noreply.github.com>
2022-01-01 12:18:22 -0800

    nice demo columns

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import OptionsMenu from './OptionsMenu';
 import ButtonFile from './ButtonFile';
 import ButtonSave from './ButtonSave';
@@ -7,6 +9,7 @@ import Conduits from './Conduits';
 import funcx from "../ncrs_scs.js";
 import data from '../data/nrcs_scs.json';
 import {Button} from 'react-bootstrap'
+import ScsTypeDropdown from './ScsTypeDropdown';
 
 
 function Model({model = {}, title='', onUpdate = f => f}) {
@@ -15,9 +18,18 @@ function Model({model = {}, title='', onUpdate = f => f}) {
   <>
     <Container className='p-3'>
       <Container className='p-5 mb-4 bg-dark rounded-3'>
-        <ButtonFile model={model} onUpdate={onUpdate}></ButtonFile>
-        <ButtonSave model={model} onUpdate={onUpdate}></ButtonSave>
-        <Button onClick={f=>{funcx(data, "I", 6, model, onUpdate); /*console.log(JSON.stringify(model, null, 2))*/}}>Calc</Button>
+        <Row className='justify-content-md-center'>
+          <Col xs lg='2'>
+            <ButtonFile model={model} onUpdate={onUpdate}></ButtonFile>
+          </Col>
+          <Col xs lg='2'>
+            <ButtonSave model={model} onUpdate={onUpdate}></ButtonSave>
+          </Col>
+          <Col xs lg='2'>
+            <Button onClick={f=>{ funcx(data, "I", 6, model, onUpdate); }}>Calc</Button>
+          </Col>
+        </Row>
+        <ScsTypeDropdown model={model}></ScsTypeDropdown>
       </Container>
     </Container>
   </>
---
+++ src/components/ScsTypeDropdown.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap'
+
+function ScsTypeDropdown({data = {}, onUpdate, style = {}}) {
+
+  const options = [
+    'I', 
+    'Ia',
+    'II',
+    'III'
+  ]
+
+  function setType(e) {
+
+  }
+
+  return (
+    <>
+      <label htmlFor="ScsType" style={style, {color:'white', width: '50%'}}>SCS Type: 
+        <DropdownButton style={{margin: '5px', width: '100%'}}
+          className={"btn-block"}
+          name="ScsType"
+          onChange={e=>setType(e.target.value)}
+        >
+          {options.map((option, i) => <Dropdown.Item key={i} value={option}>{option}</Dropdown.Item>)}
+        </DropdownButton>
+      </label>
+    </>
+  )
+}
+
+export default ScsTypeDropdown;
